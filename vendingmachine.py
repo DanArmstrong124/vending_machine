1b1870b37ea68166a8e3354f7560c3817db0e097
Ubuntu <ubuntu@ip-172-31-45-246.us-east-2.compute.internal>
2019-09-12 14:45:24 +0000

    Added Tests

--- vendingmachine.py
+++ vendingmachine.py
@@ -1,1 +1,29 @@
-coin_denomination = [100,50,20,10,5,2,1]
+from framework import *
+
+denominations = [100, 50, 20, 10, 5, 2, 1]
+
+def get_change(amount):
+    if amount == 0:
+        return []
+    
+    if amount in denominations:
+        return [amount]
+    
+    change = []
+    for coin in denominations:
+        if coin <= amount:
+            amount -= coin
+            change.append(coin)
+            
+    return change
+
+test_are_equal(get_change(0), [])
+test_are_equal(get_change(1), [1])
+test_are_equal(get_change(2), [2])
+test_are_equal(get_change(5), [5])
+test_are_equal(get_change(10), [10])
+test_are_equal(get_change(20), [20])
+test_are_equal(get_change(50), [50])
+test_are_equal(get_change(100), [100])
+test_are_equal(get_change(3), [2,1])
+test_are_equal(get_change(7), [5,2])
