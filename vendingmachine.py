90326f015e63737fe2f5330ff88ca0942b42fb04
Ubuntu <ubuntu@ip-172-31-45-246.us-east-2.compute.internal>
2019-09-12 15:18:11 +0000

    Reverted-Backup

--- vendingmachine.py
+++ vendingmachine.py
@@ -2,18 +2,13 @@ from framework import *
 
 usd_coins = [100, 50, 25, 10, 5, 1]
 eur_coins = [100, 50, 20, 10, 5, 2, 1]
-usd_coins_left = [0, 1, 2, 1, 2, 5]
-eur_coins_left = [1, 0, 2, 0, 0, 3, 4]
 
-def get_change(amount, coins=eur_coins, coins_left=eur_coins_left):
+def get_change(amount, coins=eur_coins):
     change = []
-    coin_amount = 0
     for coin in coins:
-        while coins_left[2] > 0:
-            while coin <= amount:
-                amount -= coin
-                change.append(coin)
-                coin_amount += 1
+        while coin <= amount:
+            amount -= coin
+            change.append(coin)
             
     return change
 
@@ -29,4 +24,3 @@ test_are_equal(get_change(3), [2,1])
 test_are_equal(get_change(7), [5,2])
 test_are_equal(get_change(9), [5,2,2])
 test_are_equal(get_change(35, usd_coins), [25,10])
-test_are_equal(get_change(50), [20, 20, 2, 2, 2, 1, 1, 1, 2])
